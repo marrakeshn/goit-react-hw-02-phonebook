83ce750c411b1c5b7df258be111eb3840ac3228f
marrakeshn <nesveteymark@gmail.com>
2023-04-14 01:12:51 +0300

    add del function

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id}>
+        <p>
+            {name}............
+            {number}
+        </p>
+        <button type="submit" onClick={() => onDeleteContact(id)}>
+            Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default ContactList;
